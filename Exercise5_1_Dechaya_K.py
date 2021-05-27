46c3e87826f1fb9e29b5bbebfbdc3e502c4bf9ad
Moonalish <84704274+Moonalish@users.noreply.github.com>
2021-05-27 14:04:40 +0700

    กำหนดตัวแปรบวกลบคูณหาร

---
+++ Exercise5_1_Dechaya_K.py
@@ -0,0 +1,7 @@
+# กำหนดตัวเลขบวกลบคุณหาร
+x = float(input('NUMBER1 :'))
+y = float(input('NUMBER2 :'))
+print(x,"+",y,"=",x+y)
+print(x,"-",y,"=",x-y)
+print(x,"*",y,"=",x*y)
+print(x,"/",y,"=",x/y)
